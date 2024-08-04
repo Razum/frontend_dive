fbb876a2c2ae6e238c92a71bcae62205f120cefe
Razum <razumyeyenko@gmail.com>
2024-08-04 18:56:43 +0300

    grid-area

---
+++ src/app/grid-area/page.tsx
@@ -0,0 +1,51 @@
+import clsx from "clsx";
+import styles from '@/app/grid-area/page.module.css';
+
+const GridAreaPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={clsx(styles.grid, styles.grid1)}>
+                <div className={styles.header}>Header</div>
+                <div className={styles.sidebar}>Sidebar</div>
+                <div className={styles.content}>Content</div>
+                <div className={styles.footer}>Footer</div>
+            </div>
+            <div className={clsx(styles.grid, styles.grid2)}>
+                <div className={styles.header}>Header</div>
+                <div className={styles.sidebar}>Sidebar</div>
+                <div className={styles.content}>Content</div>
+                <div className={styles.footer}>Footer</div>
+            </div>
+            <div className={clsx(styles.grid, styles.grid3)}>
+                <div className={styles.header}>Header</div>
+                <div className={styles.sidebar}>Sidebar</div>
+                <div className={styles.content}>Content</div>
+                <div className={styles.footer}>Footer</div>
+            </div>
+            <div className={clsx(styles.grid, styles.grid4)}>
+                <div className={styles.header}>Header</div>
+                <div className={styles.sidebar}>Sidebar</div>
+                <div className={styles.content}>Content</div>
+                <div className={styles.footer}>Footer</div>
+            </div>
+            <div className={clsx(styles.grid, styles.grid5)}>
+                <div className={styles.header}>Header</div>
+                <div className={styles.content}>Content</div>
+                <div className={styles.footer}>Footer</div>
+            </div>
+            <div className={clsx(styles.grid, styles.grid6)}>
+                <div>Column 1</div>
+                <div>Column 2</div>
+                <div>Column 3</div>
+            </div>
+            <div className={clsx(styles.grid, styles.grid7)}>
+                <div>Column 1</div>
+                <div>Column 2</div>
+                <div>Column 3</div>
+                <div>Column 4</div>
+            </div>
+        </div>
+    );
+};
+
+export default GridAreaPage;
